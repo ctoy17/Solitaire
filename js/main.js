b38c565707d8c67bb329f46e887c769a3330681b
ctoy17 <csharrow1994@gmail.com>
2022-04-13 18:30:08 -0700

    edit js functions

--- js/main.js
+++ js/main.js
@@ -1,150 +1,221 @@
-class Card {
-    constructor(suit, rank, value) {
-        this.suit = suit;
-        this.rank = rank;
-        this.value = value;
-    }
-}
-class Deck {
-    constructor() {
-        this.cards = [];    
-    }      
-    createDeck() {
-        let suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-        let ranks = ['ace', '2', '3', '4', '5', '6', '7', '8', '9',     '10', 'jack', 'queen', 'king'];
-        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-        for (let i = 0; i < suits.length; i++) {
-            for (let j = 0; j < ranks.length; j++) {
-                this.cards.push(new Card(suits[i], ranks[j], values[j]));
-            }
-        }
-    }
-    shuffleDeck() {
-       let location1, location2, tmp;
-       for (let i = 0; i < 1000; i++) {
-           location1 = Math.floor((Math.random() * this.cards.length));
-           location2 = Math.floor((Math.random() * this.cards.length));
-        tmp = this.cards[location1];
-        this.cards[location1] = this.cards[location2];
-        this.cards[location2] = tmp;
-        }
-    }
+// let deck1 = [] 
+// let deck2 = [] 
+const suits = ["spades", "diamonds", "clubs", "hearts"];
+const values = ["A", "02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K"];
+const rank = ["A", "r02", "r03", "r04", "r05", "r06", "r07", "r08", "r09", "r10", "J", "Q", "K"];
+
+// // Cached element references 
+let deck1El = document.getElementById('deck1') 
+let deck2El = document.getElementById('deck2')
+
+
+
+
+init(); 
+
+// Initialize deck 1 with array of 52 cards 
+function init() {
+    deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+} console.log(deck1);
+
+function render() {
+    deck.forEach(function(card, ind) {
+        deck1[ind].style.backgroundImage= `url("images/${suits}/${suits}-${rank}.svg")`
+    });
 }
 
+ // build stock pile
+ var s = [];
+
+ // build waste pile
+ var w = [];
+
+ // build foundations
+ var spades = [];
+ var hearts = [];
+ var diamonds = [];
+ var clubs = [];
+
+ // build tableau
+ var t = [];
+ t[1] = t[2] = t[3] = t[4] = t[5] = t[6] = t[7] = [];
+
+ // build table
+ var game = [];
+ game['stock'] = s;
+ game['waste'] = w;
+ game['spades'] = spades;
+ game['hearts'] = hearts;
+ game['diamonds'] = diamonds;
+ game['clubs'] = clubs;
+ game['stack'] = t;
+
+ // initial face up cards
+ var playedCards =
+ '#waste .card,' +
+ '#aces .card,' +
+ '#stack .card:last-child';
+
+//  // cache selectors
+//  var $timer = d.querySelector('#score .timer');
+//  var $timerSpan = d.querySelector('#score .timer span');
+//  var $moveCount = d.querySelector('#score .move-count');
+//  var $moveCountSpan = d.querySelector('#score .move-count span');
+//  var $score = d.querySelector('#score .score');
+//  var $scoreSpan = d.querySelector('#score .score span');
+//  var $playPause = d.querySelector('#play-pause');
+//  var $table = d.querySelector('#table');
+//  var $upper = d.querySelector('#table .upper-row');
+//  var $lower = d.querySelector('#table .lower-row');
+//  var $stock = d.querySelector('#stock');
+//  var $waste = d.querySelector('#waste');
+//  var $fnd = d.querySelector('#fnd');
+//  var $tab = d.querySelector('#tab');
+//  var $autoWin = d.querySelector('#auto-win');
+
+//  // other global vars
+//  var clock = 0;
+//  var time = 0;
+//  var moves = 0;
+//  var score = 0;
+//  var bonus = 0;
+//  var lastEventTime = 0;
+//  var unplayedTabCards = [];
 
 
+ game = deal(deck, game);
 
+// // 4. RENDER TABLE
+   render(game, playedCards);
 
+// // 5. START GAMEPLAY
+play(game);
 
+// // ### EVENT HANDLERS ###
+   window.onresize = function(event) {
+      sizeCards();
+   };
+   function deal(deck, game) {
+    console.log('Dealing Deck...');
+    // move all cards to stock
+    game['stock'] = deck;
+    // build tableau
+       var tabs = game['tableau'];
+       // loop through 7 tableau rows
+       for (var row = 1; row <= 7; row++) {
+          // loop through 7 piles in row
+          for (var pile = row; pile <= 7; pile++) {
+             // build blank pile on first row
+             if (row === 1) tabs[pile] = [];
+             // deal card to pile
+             move(game['stock'], tabs[pile], false);
+          }
+       }
+    return game;
+ }
 
-// //Cards
-// const suits = ['']
-
-
-
-
-
-
-
-
-// class Card {
-//     constructor(suit, rank, value){
-//         this.suit = suit;
-//         this.rank = rank;
-//         this.value = value;
+// // move card
+//  function move(source, dest, pop, selectedCards = 1) {
+//     if (pop !== true) {
+//        var card = source.shift(); // take card from bottom
+//        dest.push(card); // push card to destination pile
+//     } else {
+//        while (selectedCards) {
+//           // take card from the top of selection
+//           var card = source[source.length - selectedCards];
+//           // remove it from the selected pile
+//           source.splice(source.length - selectedCards, 1);
+//           // put it in the destination pile
+//           dest.push(card);
+//           // decrement
+//           selectedCards--; 
+//        }
 //     }
-// }
-// //Deck
-// class Deck {
-//     constructor(){
-//         this.cards = [];
+//     return;
+//  }
+
+// render table
+ function render(game, playedCards) {
+    console.log('Rendering Table...');
+
+    // check for played cards
+    playedCards = checkForPlayedCards(playedCards);
+
+    // check for empty piles
+    emptyPiles = checkForEmptyPiles(game);
+
+    // update stock pile
+    update(gmae['stock'], '#draw ul', playedCards, true);
+    // update waste pile
+    update(game['stock'], '#waste ul', playedCards);
+    // update spades pile
+    update(game['spades'], '#spades ul', playedCards);
+    // update hearts pile
+    update(game['hearts'], '#hearts ul', playedCards);
+    // update diamonds pile
+    update(game['diamonds'], '#diamonds ul', playedCards);
+    // update clubs pile
+    update(game['clubs'], '#clubs ul', playedCards);
+    // update tableau
+    var tabs = game['tableau'];
+    // loop through tableau piles
+    for (var i = 1; i <= 7; i++) {
+       // update tableau pile
+       update(tabs[i], '#stack li:nth-child('+i+') ul', playedCards, true);
+    }
+
+}
+
+//     // get unplayed tab cards
+//     unplayedTabCards = getUnplayedTabCards();
+
+//     // size cards
+//     sizeCards();
+
+//     // show table
+//     $game.style.opacity = '100';
+
+//     console.log('Table Rendered:', table);
+//     return;
+//  }
+
+// // update piles
+//  function update(pile, selector, playedCards, append) {
+//     var e = d.querySelector(selector);
+//     var children = e.children; // get children
+//     var grandParent = e.parentElement.parentElement; // get grand parent
+//     // reset pile
+//     e.innerHTML = '';
+//     // loop through cards in pile
+//     for (var card in pile) {
+//        card = pile[card];
+//        // get html template for card
+//        var html = getTemplate(card);
+//        // create card in pile
+//        createCard(card, selector, html, append);
 //     }
-//     createDeck (){
-//         let suits = ['diamonds', 'hearts', 'spades', 'clubs'];
-//         let ranks = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
-//         let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-//         let colors = {'diamonds': 'red', 'hearts':  'red', 'spades': 'black', 'clubs': 'black'}
-//         /* Create the deck of cards by creating each individual cards;
-//         iterate through suits, ranks, and values to create all 13 cards for each suit
-//          */
-//         for (let i=0; i < suits.length; i++){
-//             for (let n=0; n< ranks.length; n++){
-//                 this.cards.push(new Card(suits[i], ranks[n], values[n], colors[suits]));
-//             }
-//         } return Deck;
+//     // turn cards face up
+//     flipCards(playedCards, 'up');
+//     // count played cards
+//     var played = countPlayedCards(children);
+//     e.parentElement.dataset.played = played;
+//     // count all played cards for #tab and #fnd piles
+//     if ( grandParent.id === 'tab' || grandParent.id === 'fnd' ) {
+//        var playedAll = parseInt(grandParent.dataset.played);
+//        if ( isNaN(playedAll) ) playedAll = 0;
+//        grandParent.dataset.played = playedAll + played;
 //     }
-//     shuffleDeck() {
-//         var i = Deck.length, temp, rand;
-//         while (0 !== i) {
-//            rand = Math.floor(Math.random() * i); i--;
-//         temp = Deck[i];
-//         Deck[i] = Deck[rand];
-//         Deck[rand] = temp;
-//         }
-//         return Deck;
+//     // count unplayed cards
+//     var unplayed = countUnplayedCards(children);
+//     e.parentElement.dataset.unplayed = unplayed;
+//     // count all unplayed cards for #tab and #fnd piles
+//     if ( grandParent.id === 'tab' || grandParent.id === 'fnd' ) {
+//        var unplayedAll = parseInt(grandParent.dataset.unplayed);
+//        if ( isNaN(unplayedAll) ) unplayedAll = 0;
+//        grandParent.dataset.unplayed = unplayedAll + unplayed;
 //     }
-// }
+//     return pile;
+//  }
 
-// let $gameScreen = document.getElementById($gameScreen);
-// let deck1 = [];
-// let deck2 = [];
 
-// let deck1El = document.getElementById('deck1') 
-// let deck2El = document.getElementById('deck2')
-
-// document.getElementById('btn').addEventListener('click', ()=> console.log('clicked'))
-// // function renderGame(){
-// // Initialize deck 1 with array of 52 cards 
-// function init() { 
-//     deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","h
-//   A","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","c
-//   J","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s0
-//   9","s08","s07","s06","s05","s04","s03","s02"]
-// }
-
-// function handleClick() { 
- 
-//     // Used to prevent error on click when no cards are left in deck 1 
-//     if (deck1.length > 0) {   
-   
-//       // Randomly select number from total cards remaining 
-//       let randIdx = Math.floor(Math.random()*deck1.length) 
-   
-//       // Assigns card with the random index to a variable    
-//       cardPicked = deck1.splice(randIdx, 1)  
-   
-//       // Adds card picked to deck 2 
-//       deck2.push(cardPicked)  
-   
-//       // Pass card picked to render function to display 
-//       render(cardPicked) 
-//     } 
-//   }
-
-// // }
-// // //game area
-// // //draw pile
-// // var d = [];
-
-// // //waste pile
-// // var w = [];
-
-// // //aces
-// // var diamonds = [];
-// // var hearts = [];
-// // var spades = [];
-// // var clubs = [];
-
-// // //tableau
-// // var t = [];
-// // t[1] = t[2] = t[3] = t[4] = t[5] = t[6] = t[7] = [];
-
-// // var table = [];
-// // table['draw'] = d;
-// // table['waste'] = w;
-// // table['diamonds'] = diamonds;
-// // table['hearts'] = hearts;
-// // table['spades'] = spades;
-// // table['clubs'] = clubs;
-// // table['tableau'] = t;
-
+window.addEventListener('load', load);
